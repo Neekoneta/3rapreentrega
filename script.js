6f35c13d3cccfd802a978793e6264999ddba2ddd
Neekoneta <stefamanzanelli@gmail.com>
2024-09-24 18:45:50 -0300

    proyectofinal

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const juegos = [
-        {
-            id: 1,
-            nombre: "Red Dead Redemption 2",
-            precio: 259.99,
-            imagen: "https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png"
-        },
-        {
-            id: 2,
-            nombre: "God Of War Ragnarok",
-            precio: 149.99,
-            imagen: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png"
-        },
-        {
-            id: 3,
-            nombre: "Howarts Legacy",
-            precio: 9.99,
-            imagen: "https://image.api.playstation.com/vulcan/ap/rnd/202208/0921/Ah7Ar9MU0r1BBlzAUflmhyQP.png"
-        },
-        {
-            id: 4,
-            nombre: "Far Cry 6",
-            precio: 0.99,
-            imagen: "https://image.api.playstation.com/vulcan/img/rnd/202106/0722/4MxzDZKZwtEWyMWZghvwd7bQ.jpg"
-        },
-        {
-            id: 5,
-            nombre: "CyberPunk 2077",
-            precio: 20.77,
-            imagen: "https://image.api.playstation.com/vulcan/ap/rnd/202111/3013/2tW2zf7n8wwwWXZO9dAQMVu5.png"
-        }
-    ];
-
     const juegosContainer = document.getElementById('juegos-container');
     const listaJuegos = document.getElementById('lista-juegos');
     const carritoLista = document.getElementById('carrito-lista');
@@ -40,6 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const comprarBtn = document.getElementById('comprar');
 
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    let juegos = [];
+
+    async function cargarJuegos() {
+        try {
+            const respuesta = await fetch('juegos.json');
+            juegos = await respuesta.json();
+            mostrarJuegos();
+        } catch (error) {
+            console.error("Error al cargar los juegos:", error);
+        }
+    }
 
     function mostrarJuegos() {
         juegos.forEach(juego => {
@@ -97,15 +75,16 @@ document.addEventListener('DOMContentLoaded', () => {
               });
             // alert(`Has comprado ${cantidadJuegos} juegos por un total de $${total.toFixed(2)}`);
             vaciarCarrito();
-        } else {
+            } else {
             Swal.fire({
                 icon: "error",
                 title: "El carrito esta vacio",
                 text: "Añade algo al carrito para comprar",
               });
-        }
+            }
     }
 
+   
     juegosContainer.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
             const id = e.target.getAttribute('data-id');
@@ -115,7 +94,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
     comprarBtn.addEventListener('click', comprar);
-
-    mostrarJuegos();
+    cargarJuegos();
     actualizarCarrito();
 });
+
+
+
+
+
+
+
